72aae41c0fcccb2700a1d90e1be89d6797d73e5b
Sasha <ponomarek91@gmail.com>
2020-03-27 14:32:46 +0200

    Updates

---
+++ precache-manifest.a13f4054ad722cc322059b39d5e2e719.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "791677ea50f68f175290fdfe525af766",
+    "url": "/15-Homework/index.html"
+  },
+  {
+    "revision": "f42ff3908686ffcbd7b6",
+    "url": "/15-Homework/static/css/main.4fa361dd.chunk.css"
+  },
+  {
+    "revision": "cc5b60f530ec9ef2959d",
+    "url": "/15-Homework/static/js/2.3dd64d21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/15-Homework/static/js/2.3dd64d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f42ff3908686ffcbd7b6",
+    "url": "/15-Homework/static/js/main.5d0f6d5a.chunk.js"
+  },
+  {
+    "revision": "22e4685f7d4c87c77b03",
+    "url": "/15-Homework/static/js/runtime-main.f5d5ccd9.js"
+  }
+]);
